c90942f41a91d771aaac13528aee8bcb72e5f11c
dodi258 <dodi258@naver.com>
2021-02-28 23:35:22 +0900

    1. spock, lombok, db 설정/ 2.Customer 클래스 생성/ 3.spock test 생성

--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -1,2 +1,43 @@
-package entity;public class Customer {
+package entity;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
 }
--- src/main/java/service/CustomerReader.java
+++ src/main/java/service/CustomerReader.java
@@ -1,2 +1,19 @@
-package service;public class CustomerReader {
+package service;
+
+import entity.Customer;
+import lombok.Setter;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Setter
+public class CustomerReader {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public String findFullName(Long customerID){
+        Customer customer = entityManager.find(Customer.class, customerID);
+        return customer.getFirstName() +" "+customer.getLastName();
+    }
 }
